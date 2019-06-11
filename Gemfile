d7b5d2ede49d8d769de8265205d76a047e1f4a1a
Craig_Donavin <crdonavin@gmail.com>
2019-06-10 22:01:21 -0500

    Pseudo code notes added

--- Gemfile
+++ Gemfile
@@ -2,5 +2,6 @@ source "https://rubygems.org"
 
 gem "nokogiri"
 #gem "open-uri"
+
 # Specify your gem's dependencies in cli_project.gemspec
 gemspec 
--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,11 +5,20 @@ class Scraper
 
 html = open('https://www.velocityusa.com/product/rim')
 
-    def application_scraper(html)
+    def self.application_scraper(html)
         doc = Nokogiri::HTML(html)
         binding.pry
-        app_box = doc.css('#application.content')
-        
+        application = doc.css('#application.content').children.css("#wh-use.wh-applications")
+        #i'll likely have to condense the contents of these to an array. How do I capture the descriptor name without the link?
+        #then use detector methods to populate an array. bonus: i shouldn't have any hard-coded keys/elements. 
     end
-
+    
+    #the product url is found in "#wh-use.wh-applications a". I could throw that directly in the argument, but i feel like there's a better way
+    def self.rim_scraper(product_url)
+        spec_hash = {}
+        #create the container
+        #comb the page, then the element
+        # iterate the element to populate the hash
+        #color and spoke count will both have arrays as values, so once the keys are made, shovel the choices into the waiting containers
+    end
 end
